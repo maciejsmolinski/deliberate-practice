922de05cba9eb263a67969c85214d69e962aa9cd
Maciej Smolinski <contact@maciejsmolinski.com>
2020-10-24 22:36:06 +0200

    chore: add description in jsdoc to `task/delayed`

--- src/task.ts
+++ src/task.ts
@@ -2,5 +2,8 @@ import { task, of, ITask } from 'folktale/concurrency/task';
 
 export { task, of };
 
+/**
+ * Returns a task which resolves successfully after given timeout
+ */
 export const delayed = <T>(value: T, timeout: number): ITask<T> =>
   task(resolver => setTimeout(() => resolver.resolve(value), timeout));
