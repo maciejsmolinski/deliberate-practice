91aeff76aef412ad2b6c5400c9c8587208050cca
Maciej Smolinski <contact@maciejsmolinski.com>
2020-10-24 22:33:03 +0200

    feat: render application contents w/ bumbag ui kit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import * as ReactDOM from 'react-dom';
 import { delayed } from './task';
+import { Heading, Box, Container, Provider as BumbagProvider } from 'bumbag';
 
 function Main() {
   const [message, setMessage] = useState('Main Component!');
@@ -14,7 +15,15 @@ function Main() {
     [message],
   );
 
-  return <div>{message}</div>;
+  return (
+    <BumbagProvider>
+      <Container>
+        <Box padding="1rem 0.5rem">
+          <Heading use="h3">{message}</Heading>
+        </Box>
+      </Container>
+    </BumbagProvider>
+  );
 }
 
 ReactDOM.render(<Main />, document.getElementById('app'));
