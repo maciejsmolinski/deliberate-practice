02a0863f520a1bc3546886aef9973052abafb10f
Maciej Smolinski <contact@maciejsmolinski.com>
2020-10-27 00:46:30 +0100

    style(Main): increase spacing between header and contents

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Heading } from 'bumbag';
+import { Heading, Stack } from 'bumbag';
 import Container from './components/Container';
 import Board from './components/Board';
 import useBoard from './hooks/useBoard';
@@ -10,10 +10,10 @@ function Main() {
 
   return (
     <Container>
-      <>
+      <Stack spacing="major-4">
         <Heading use="h3">Deliberate Practice</Heading>
         <Board cards={cards} />
-      </>
+      </Stack>
     </Container>
   );
 }
