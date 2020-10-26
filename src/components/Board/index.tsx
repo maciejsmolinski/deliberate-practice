88baab1b4ff75426b9ccbb28d2353155e456e540
Maciej Smolinski <contact@maciejsmolinski.com>
2020-10-26 22:52:05 +0100

    feat(Board): render some cards on the board

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Columns } from 'bumbag';
+import { Columns, Card } from 'bumbag';
 
 export default function Board() {
-    return (
-        <Columns>
-            <Columns.Column>Can&apos;t do</Columns.Column>
-            <Columns.Column>Can do with effort</Columns.Column>
-            <Columns.Column>Mastered/Automated</Columns.Column>
-        </Columns>
-    );
+  return (
+    <Columns>
+      <Columns.Column>
+        Can&apos;t do
+        <Card>Property Based Testing</Card>
+        <Card>SProxy - Type level programming</Card>
+      </Columns.Column>
+      <Columns.Column>
+        Can do with effort
+        <Card>Unit Tests</Card>
+      </Columns.Column>
+      <Columns.Column>
+        Mastered/Automated
+        <Card>FFI</Card>
+        <Card>JSON Encoding/Decoding</Card>
+      </Columns.Column>
+    </Columns>
+  );
 }
