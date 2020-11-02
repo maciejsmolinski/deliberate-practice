238dc534ff92913d2084ebf84b40cb65ad58fb61
Maciej Smolinski <contact@maciejsmolinski.com>
2020-11-02 23:38:11 +0100

    feat: define Board/Card/Category schema

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+  enum Category {
+    CannotDo
+    CanDoWithEffort
+    Mastered
+  }
+
+  type Card {
+    title: String!
+    status: Category!
+  }
+
+  type Board {
+    cards: [Card!]
+  }
+
   type Query {
     "Query to retrieve all deliberate practice exercises"
-    test: String
+    board: Board
   }
 `;
 
 const resolvers = {
   Query: {
-    test() {
-      return 'Test';
+    board() {
+      return {
+        cards: [
+          {
+            status: 'CannotDo',
+            title: 'Skill to learn',
+          },
+          { status: 'Mastered', title: 'Mastered skill' },
+        ],
+      };
     },
   },
 };
